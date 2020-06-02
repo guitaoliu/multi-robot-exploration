ff0c908fcac6d7ff17fe29355f4522aa58f9f3c0
Antimony5292 <32815641+Antimony5292@users.noreply.github.com>
2020-06-02 17:28:23 +0800

    Update GUI.py
    using const to define zoom

--- robots/GUI.py
+++ robots/GUI.py
@@ -4,16 +4,18 @@ from setting import (
     MAP_SIZE,
 )
 
+ZOOM_SIZE=5 # 地图缩放倍率
+
 def BeginSim():
     '''将主函数移植进来'''
     explore_map=[0,0,0,0,1,1,1,1,1,1,1]
-    barrier_map=[0,0,0,0,0,0,0,1,1,0,0]
+    barrier_map=[0,0,0,0,0,0,0,1,1,0,0] # just a example
     for i in range(0,len(explore_map)):
         if explore_map[i]==1:
             if barrier_map[i]==1:
-                canvas.create_rectangle(5*i//MAP_SIZE[0],5*i%MAP_SIZE[1],5*i//MAP_SIZE[0]+5,5*i%MAP_SIZE[1]+5,fill='white')
+                canvas.create_rectangle(ZOOM_SIZE*i//MAP_SIZE[0],ZOOM_SIZE*i%MAP_SIZE[1],ZOOM_SIZE*i//MAP_SIZE[0]+ZOOM_SIZE,ZOOM_SIZE*i%MAP_SIZE[1]+ZOOM_SIZE,fill='white')
             else:
-                canvas.create_rectangle(5*i//MAP_SIZE[0],5*i%MAP_SIZE[1],5*i//MAP_SIZE[0]+5,5*i%MAP_SIZE[1]+5,fill='black')
+                canvas.create_rectangle(ZOOM_SIZE*i//MAP_SIZE[0],ZOOM_SIZE*i%MAP_SIZE[1],ZOOM_SIZE*i//MAP_SIZE[0]+ZOOM_SIZE,ZOOM_SIZE*i%MAP_SIZE[1]+ZOOM_SIZE,fill='black')
 
 
 
@@ -24,10 +26,10 @@ MainWindow.geometry('1200x900')
 B=tk.Button(MainWindow,text='Begin Simulating',font=('Arial',12),width=15,height=1,command=BeginSim)
 B.pack()
 
-canvas=tk.Canvas(MainWindow,width=MAP_SIZE[0]*5,height=MAP_SIZE[1]*5)
+canvas=tk.Canvas(MainWindow,width=MAP_SIZE[0]*ZOOM_SIZE,height=MAP_SIZE[1]*ZOOM_SIZE)
 for i in range(0,MAP_SIZE[0]):
     for j in range(0,MAP_SIZE[1]):
-        canvas.create_rectangle(5*i,5*j,5*(i+1),5*(j+1),fill='blue')
+        canvas.create_rectangle(ZOOM_SIZE*i,ZOOM_SIZE*j,ZOOM_SIZE*(i+1),ZOOM_SIZE*(j+1),fill='blue')
 canvas.pack()
 
 MainWindow.mainloop()
